29544753c0f42798925917e12f390cf092b2b15c
James <james.c.hockey@gmail.com>
2015-03-26 23:33:17 +0800

    Added lcm

--- notebook-source/Math/gcd.java
+++ notebook-source/Math/gcd.java
@@ -4,18 +4,18 @@ class gcd{
 
 /**
  * Greatest common denominator using euclids algorithm.
+ * And LCM
  */
 
 
 /* START SOLUTION */
-public static long gcd(long a, long b) {
-	return (b == 0) ? a : gcd(b, a % b);
-}
+static long gcd(long a, long b) { return (b == 0) ? a : gcd(b, a % b); }
+static long lcm(long a, long b) { return a * (b / gcd(a, b)); }
 /* END SOLUTION */
 
 
 public static void main(String[] args) {
-	System.out.println(gcd(150, 100));
+	System.out.println("150, 100\ngcd: " + gcd(150, 100) + "\tlcm: " + lcm(150, 100));
 }	
 
 
